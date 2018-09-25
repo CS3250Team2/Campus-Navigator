a987157044be05878fd7963e61c6325eeb58a101
projectGroup3250Team <jtreece3@msudenver.edu>
2018-09-24 20:35:11 -0600

    Got it working just have to launch the server file seperate from the app.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import UserForm from './components/userForm';
 import registerServiceWorker from './registerServiceWorker';
 
