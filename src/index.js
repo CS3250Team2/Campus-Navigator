ef4ff072e8fd13393abf55d25783af1d56352391
projectGroup3250Team <jtreece3@msudenver.edu>
2018-09-24 20:02:30 -0600

    Non-working frontend to backend bufix commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UserForm from './components/userForm';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root')); //THIS LINE IS WHAT RENDERS THE APPLICATION TO THE HTML DOM
+ReactDOM.render(<UserForm />, document.getElementById('root')); //THIS LINE IS WHAT RENDERS THE APPLICATION TO THE HTML DOM
 
 
 
