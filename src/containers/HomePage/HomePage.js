4a976e16d90e0689fff3b756bb0500fc0a2673b7
Ryan Hiller <ryanhiller@growhousegaming.com>
2018-10-02 15:56:31 -0600

    Continued work on main page

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -5,7 +5,10 @@ import classes from './HomePage.css';
 class HomePage extends Component {
     render() {
         return(
-            <div className={classes.HomePage}>HomePage Content Test</div>
+            <div className={classes.HomePage}>
+                <h1>LET'S GET YOU<br />ON YOUR WAY</h1>
+                <div className="uploadButton">UPLOAD SCHEDULE</div>
+            </div>
         );
     }
 }
