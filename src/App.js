d09052acefdfb7168fa8fcc27d8c5f41ba515c48
Ryan Hiller <ryanhiller@growhousegaming.com>
2018-10-24 12:08:39 -0600

    Current homepage, register, and login pages all routed

--- src/App.js
+++ src/App.js
@@ -5,24 +5,30 @@
  *
  */
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 //import './bootstrap-4.1.3-dist/bootstrap.min.css'
 
-
-import classes from './App.css';
-import Layout from './components/Layout/Layout';
-import HomePage from './containers/HomePage/HomePage';
+import classes from "./App.css";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./containers/HomePage/HomePage";
+import Register from "./containers/Register/Register";
+import Login from "./containers/Login/Login";
 
 class App extends Component {
-  render() {
-    return (
-      <div className={classes.App}>
-        <Layout>
-          <HomePage/>
-        </Layout>
-      </div>
-    );
-  };
+    render() {
+        return (
+            <div className={classes.App}>
+                <Router>
+                    <Layout>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/register" exact component={Register} />
+                        <Route path="/login" exact component={Login} />
+                    </Layout>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
