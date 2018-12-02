7f50abec26ce90b8b9f14748ee76b9d11397098a
Pashweetie <Pashweetie@gmail.com>
2018-12-02 11:55:56 -0700

    Its a start

--- src/containers/EventPage/EventPage.js
+++ src/containers/EventPage/EventPage.js
@@ -7,16 +7,54 @@
 
  import classes from "./EventPage.css";
 
- class EventPage extends Component {
+ class Blog extends Component {
+   constructor(){
+     super();
+     this.state ={
+       recentBlogPost:{
+         name:'';
+         url:'';
+       }
+     }
+   }
+   FetchDataFromRssFeed(){
+     var request = new XMLHttpRequest();
+     request.onreadystatechange = ()={
+       if(request.readyState==4&&request.status==200){
+         var myObj = JSON.parse(request.responseTest);
+         for(let i=0;i<1;i++){
+           this.setState({
+             recentBlogPost:{
+               name:myObj.items[i].title,
+               url:myObj.items[i].link
+             }
+           });
+         }
+       }
+     }
+     request.open("GET","url",true);
+     request.send();
+   }
+   componentDidMount(){
+     {this.FetchDataFromRssFeed()}
+   }
+   render(){
+     return(
+       <div>
+         Check out our blog:<a target="_blank" href={this.state.recentBlogPost.url}>{this.state.recentBlogPost.name}</a>
+       </div>
+     );
+   }
+   export class EventPage extends Component{
      render() {
          return (
              <div className={classes.EventPage}>
                <div className={classes.Content}>
                   <h1>EVENTS</h1>
+                  <Blog/>
                </div>
              </div>
          );
      }
- }
 
- export default EventPage;
+ }
