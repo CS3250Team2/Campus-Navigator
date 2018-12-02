e3b3318d333d937522a134ab4f2c83291b03e99b
Jeremy Oliver <joliver427@icloud.com>
2018-12-02 13:58:55 -0700

    Added cors, started writing functions

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,64 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const database = admin.database().ref('/classes');
+
+exports.uploadClasses = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'POST') {
+      return res.status(401).json({
+        message: 'Not allowed'
+      })
+    }
+    console.log(req.body)
+    const item = req.body.item
+    database.push({ item });
+    let items = [];
+    return database.on('value', (snapshot) => {
+      snapshot.forEach((item) => {
+        items.push({
+          id: item.key,
+          items: item.val().items
+        });
+      });
+      res.status(200).json(items)
+    }, (error) => {
+      res.status(error.code).json({
+        message: 'Something went wrong. ${error.message}'
+      })
+    })
+  })
+})
+
+exports.getClasses = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'GET') {
+      return res.status(401).json({
+        message: 'Not allowed'
+      })
+    }
+
+    let items = [];
+
+    return database.on('value', (snapshot) => {
+      snapshot.forEach((item) => {
+        items.push({
+          id: item.key,
+          items: item.val().item
+        });
+      });
+
+      res.status(200).json(items)
+    }, (error) => {
+      res.status(error.code).json({
+        message: `Something went wrong. ${error.message}`
+      })
+    })
+  })
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
