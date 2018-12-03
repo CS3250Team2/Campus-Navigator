52f557f774fcaa63955c20b7988795d35cba9f86
Pashweetie <Pashweetie@gmail.com>
2018-12-02 18:14:41 -0700

    did i do it?

--- src/containers/EventPage/EventPage.js
+++ src/containers/EventPage/EventPage.js
@@ -6,55 +6,19 @@
  import UserForm from "../../components/UserForm/UserForm";
 
  import classes from "./EventPage.css";
+ import Blog from "../../components/Blog/Blog";
 
- class Blog extends Component {
-   constructor(){
-     super();
-     this.state ={
-       recentBlogPost:{
-         name:'';
-         url:'';
-       }
-     }
-   }
-   FetchDataFromRssFeed(){
-     var request = new XMLHttpRequest();
-     request.onreadystatechange = ()={
-       if(request.readyState==4&&request.status==200){
-         var myObj = JSON.parse(request.responseTest);
-         for(let i=0;i<1;i++){
-           this.setState({
-             recentBlogPost:{
-               name:myObj.items[i].title,
-               url:myObj.items[i].link
-             }
-           });
-         }
-       }
-     }
-     request.open("GET","https://www.trumba.com/calendars/msu-denver-events-calendars-academic-calendar-student.xml",true);
-     request.send();
-   }
-   componentDidMount(){
-     {this.FetchDataFromRssFeed()}
-   }
-   render(){
-     return(
-       <div>
-         Check out our blog:<a target="_blank" href={this.state.recentBlogPost.url}>{this.state.recentBlogPost.name}</a>
-       </div>
-     );
-   }
-   export class EventPage extends Component{
-     render() {
-         return (
-             <div className={classes.EventPage}>
-               <div className={classes.Content}>
-                  <h1>EVENTS</h1>
-                  <Blog/>
-               </div>
+
+ export class EventPage extends Component{
+   render() {
+       return (
+           <div className={classes.EventPage}>
+             <div className={classes.Content}>
+                <h1>EVENTS</h1>
+                <Blog/>
              </div>
-         );
-     }
-
+           </div>
+       );
+   }
  }
+export default EventPage;
