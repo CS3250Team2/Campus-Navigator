ea1ba2a1fc7a1962011ce72ca4c76debbf02d526
Ryan Hiller <ryanhiller@growhousegaming.com>
2018-12-04 00:11:02 -0700

    Cloud function is ready for the parser to be added to it. Currently reads the file's download link from storage, downloads it in a temporary directory, then places the file's contents in a variable.

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,34 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const htmlparser = require('htmlparser2');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
 admin.initializeApp();
-
-parser = () => {
-    
-}
 
 exports.parseHTML = functions.storage.object().onFinalize((object, context) => {
     let uid = object.name.split(/[.\/]/);
     const db = admin.database().ref(`users/${uid[1]}`);
-    
-    db.update({'schedule':'test'});
+    const fileName = path.basename(object.name);
+    const bucket = admin.storage().bucket('schedule');
+    const tempPath = path.join(os.tmpdir(), fileName);
+
+    let schedule = bucket
+        .file(fileName)
+        .download({
+            destination: tempPath,
+        })
+        .then(() => {
+            console.log('Image downloaded to', tempPath);
+            return fs.readFile(tempPath, 'utf8', (err, data) => {
+                /*PARSER LOGIC GOES HERE*/
+            });
+        })
+        .then(() => fs.unlinkSync(tempPath));
+
+    console.log();
+
+    //db.update({ schedule: 'test' });
     return 1;
 });
